06da857d0980526073ebd76a281d628137f1eeea
saltire sable <saltiresable@gmail.com>
2019-10-18 18:05:46 -0400

    Parse and print alsaseq event arguments

--- midi.py
+++ midi.py
@@ -3,9 +3,46 @@ import alsaseq
 
 CLIENT = 24
 
-alsaseq.client('Simple', 1, 1, False)
-alsaseq.connectfrom(0, CLIENT, 0)
+events = {
+    5: 'note',
+    6: 'noteon',
+    7: 'noteoff',
+    8: 'keypress',
+    10: 'controller',
+    11: 'pgmchange',
+    12: 'chanpress',
+    13: 'pitchbend',
+}
+
+
+alsaseq.client('Simple', 1, 1, False) # name, ninputports, noutputports, createqueue
+alsaseq.connectfrom(0, CLIENT, 0) # inputport, src_client, src_port
 
 while True:
     if alsaseq.inputpending():
-        print(alsaseq.input())
+        event = alsaseq.input()
+
+        (etype, # enum snd_seq_event_type
+         flags,
+         tag,
+         queue,
+         timestamp,
+         source,
+         destination,
+         data) = event
+
+        sec, msec = timestamp
+        sclient, sport = source
+        dclient, dport = destination
+
+        # print(event)
+
+        if etype in [10, 11, 12, 13]:
+            channel, _, _, _, param, value = data
+            print('{} event: channel {}, param {}, value {}'
+                .format(events[etype], channel, param, value))
+
+        else:
+            channel, note, velocity, off_velocity, duration = data
+            print('{} event: channel {}, note {}, velocity {}, off velocity {}, duration {}'
+                .format(events[etype] if etype in events else etype, *data))
